a89d6fe5b5ed73caa86acbd642e27f9f5a62f054
Mika <mika.laikko@eng.tamk.fi>
2014-01-16 14:10:46 +0200

    Kaupalle jotain pohjaa
    Roskiin vai jatkoon?

--- Assets/Scripts/kauppaScript.cs
+++ Assets/Scripts/kauppaScript.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class kauppaScript : MonoBehaviour {
 	
 	public GameObject activeBlock;
+	//public GameObject test;
 	public bool kauppaEnabled = false;
-	public bool blockSelected = false;
 	public clickScript click;
+	//List<GameObject> solut;
 	
 	// Use this for initialization
 	void Start () {
@@ -17,18 +19,36 @@ public class kauppaScript : MonoBehaviour {
 		if( kauppaEnabled == false ) {
 			click.highlightcube.renderer.enabled = true;
 			kauppaEnabled = true;
+			this.guiText.text = "Kauppa: ON";
+			
+			foreach(GameObject test in GameObject.FindGameObjectsWithTag("test"))
+			{
+        		test.guiTexture.enabled = true;
+			}
+
 		}			
 		else {
 			kauppaEnabled = false;
 			click.highlightcube.renderer.enabled =false;
+			click.highlightcube.transform.position += new Vector3(0,0,1);
+			this.guiText.text = "Kauppa: OFF";
+			
+			foreach(GameObject test in GameObject.FindGameObjectsWithTag("test"))
+			{
+			test.guiTexture.enabled = false;
+			}
 		}		
 	}
-	
+
 	public void saveActiveBlock (GameObject block) {
 		activeBlock = block;
-		Debug.Log(activeBlock.transform.position.x);
 	}
-	
+	void OnMouseOver () {
+		this.guiText.fontSize = 17;
+	}
+	void OnMouseExit () {
+		this.guiText.fontSize = 15;
+	}
 	// Update is called once per frame
 	void Update () {
 	
---
+++ Assets/Scripts/kauppaSoluScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kauppaSoluScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnMouseDown () {
+	Debug.Log("asd");
+	}
+	void OnMouseOver () {
+		this.transform.localScale = new Vector3(0.01f,0.01f,0f);
+		this.transform.localPosition = new Vector3(0f,0f,0.1f);
+	}
+	void OnMouseExit () {
+		this.transform.localScale = new Vector3(0f,0f,0f);
+		this.transform.localPosition = new Vector3(0f,0f,0f);
+	}
+}
